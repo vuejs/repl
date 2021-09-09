b651324efaff3a7644a67aa03340caa26613073b
Evan You <yyx990803@gmail.com>
2021-09-09 19:26:08 -0400

    stub in ssr

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
-import { defineConfig } from 'vite'
+import { defineConfig, Plugin } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const genStub: Plugin = {
+  name: 'gen-stub',
+  apply: 'build',
+  generateBundle() {
+    this.emitFile({
+      type: 'asset',
+      fileName: 'ssr-stub.js',
+      source: `module.exports = {}`
+    })
+  }
+}
+
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), genStub],
   build: {
     minify: false,
     lib: {
