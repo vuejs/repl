60db54905699e005d3117a693410c0cd50f154fe
三咲智子 <sxzz@sxzz.moe>
2022-01-20 18:11:52 +0800

    feat: export compileFile (#13)
    Co-authored-by: Evan You <yyx990803@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { default as Repl } from './Repl.vue'
 export { ReplStore, File } from './store'
+export { compileFile } from './transform'
 export type { Store, SFCOptions, StoreState } from './store'
