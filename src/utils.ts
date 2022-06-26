b12eb885deb080246d372495f443fe543de1eb6d
Kael <kaelwd@gmail.com>
2022-06-26 12:37:59 +1000

    feat: gzip serialized state (#43)

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,5 @@
+import { zlibSync, unzlibSync, strToU8, strFromU8 } from 'fflate'
+
 export function debounce(fn: Function, n = 100) {
   let handle: any
   return (...args: any[]) => {
@@ -8,12 +10,24 @@ export function debounce(fn: Function, n = 100) {
   }
 }
 
-// prefer old unicode hacks for backward compatibility
-// https://base64.guru/developers/javascript/examples/unicode-strings
 export function utoa(data: string): string {
-  return btoa(unescape(encodeURIComponent(data)))
+  const buffer = strToU8(data)
+  const zipped = zlibSync(buffer, { level: 9 })
+  const binary = strFromU8(zipped, true)
+  return btoa(binary)
 }
 
 export function atou(base64: string): string {
-  return decodeURIComponent(escape(atob(base64)))
+  const binary = atob(base64)
+
+  // zlib header (x78), level 9 (xDA)
+  if (binary.startsWith('\x78\xDA')) {
+    const buffer = strToU8(binary, true)
+    const unzipped = unzlibSync(buffer)
+    return strFromU8(unzipped)
+  }
+
+  // old unicode hacks for backward compatibility
+  // https://base64.guru/developers/javascript/examples/unicode-strings
+  return decodeURIComponent(escape(binary))
 }
