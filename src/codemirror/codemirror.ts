34030278518bc1d021e20ee1bb8b6b5373482379
三咲智子 <sxzz@sxzz.moe>
2023-01-03 00:03:05 +0800

    feat: add search and replace
    shortcuts: https://codemirror.net/5/demo/search.html

--- src/codemirror/codemirror.ts
+++ src/codemirror/codemirror.ts
@@ -1,4 +1,5 @@
 import CodeMirror from 'codemirror'
+import 'codemirror/addon/dialog/dialog.css'
 import './codemirror.css'
 
 // modes
@@ -15,5 +16,8 @@ import 'codemirror/addon/fold/foldgutter.js'
 import 'codemirror/addon/fold/brace-fold.js'
 import 'codemirror/addon/fold/indent-fold.js'
 import 'codemirror/addon/fold/comment-fold.js'
+import 'codemirror/addon/search/search.js'
+import 'codemirror/addon/search/searchcursor.js'
+import 'codemirror/addon/dialog/dialog.js'
 
 export default CodeMirror
