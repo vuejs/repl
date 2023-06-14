b8caeaef0368609fa3c41e992304d21d526de08c
edison <daiwei521@126.com>
2023-06-14 17:54:04 +0800

    fix: improve code with optional chain (#72)

--- src/transform.ts
+++ src/transform.ts
@@ -148,7 +148,7 @@ export async function compileFile(
     if (!clientTemplateResult) {
       return
     }
-    clientCode += clientTemplateResult
+    clientCode += `;${clientTemplateResult}`
 
     const ssrTemplateResult = await doCompileTemplate(
       store,
@@ -160,7 +160,7 @@ export async function compileFile(
     )
     if (ssrTemplateResult) {
       // ssr compile failure is fine
-      ssrCode += ssrTemplateResult
+      ssrCode += `;${ssrTemplateResult}`
     } else {
       ssrCode = `/* SSR compile error: ${store.state.errors[0]} */`
     }
