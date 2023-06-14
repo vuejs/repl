4ca3d94c98ed2029ccd61197780d45f348b2fcde
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2023-06-14 18:31:59 +0800

    feat: add search and replace (#67)
    shortcuts: https://codemirror.net/5/demo/search.html

--- src/codemirror/codemirror.ts
+++ src/codemirror/codemirror.ts
@@ -1,4 +1,5 @@
 import CodeMirror from 'codemirror'
+import 'codemirror/addon/dialog/dialog.css'
 import './codemirror.css'
 
 // modes
@@ -15,6 +16,9 @@ import 'codemirror/addon/fold/foldgutter.js'
 import 'codemirror/addon/fold/brace-fold.js'
 import 'codemirror/addon/fold/indent-fold.js'
 import 'codemirror/addon/fold/comment-fold.js'
+import 'codemirror/addon/search/search.js'
+import 'codemirror/addon/search/searchcursor.js'
+import 'codemirror/addon/dialog/dialog.js'
 
 // keymap
 import 'codemirror/keymap/sublime.js'
