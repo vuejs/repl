863f8f39d36d25240388a9c5bc68eff0ea7e7856
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2023-07-10 00:11:42 +0800

    fix: replace NODE_ENV
    removed in bfc3522 before, but in fact we need it

--- vite.preview.config.ts
+++ vite.preview.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import replace from '@rollup/plugin-replace'
 
 export default defineConfig({
   plugins: [
@@ -23,5 +24,13 @@ export default defineConfig({
   },
   worker: {
     format: 'es',
+    plugins: [
+      replace({
+        preventAssignment: true,
+        values: {
+          'process.env.NODE_ENV': JSON.stringify('production'),
+        },
+      }),
+    ],
   },
 })
