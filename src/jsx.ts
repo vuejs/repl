48325f99e010c3065c99efd4fb3e95950cda9596
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2025-01-02 23:32:56 +0800

    fix: transform jsx for entire file
    closes #301, closes #300

--- src/jsx.ts
+++ src/jsx.ts
@@ -1,7 +1,7 @@
 import { transform } from '@babel/standalone'
 import jsx from '@vue/babel-plugin-jsx'
 
-export async function transformJSX(src: string) {
+export function transformJSX(src: string) {
   return transform(src, {
     plugins: [jsx],
   }).code!
