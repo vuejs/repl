f113e2887d4abcff10c91fe6b6557a9cba9fdd36
Dsaquel <noblet.ouwaysgta5@gmail.com>
2025-01-25 00:55:01 +0100

    chore: move all tsx able to core.ts

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,3 @@
 export { default as Repl } from './Repl.vue'
 export { default as Preview } from './output/Preview.vue'
-export {
-  useStore,
-  File,
-  type SFCOptions,
-  type StoreState,
-  type Store,
-  type ReplStore,
-} from './store'
-export { useVueImportMap, mergeImportMap, type ImportMap } from './import-map'
-export { compileFile } from './transform'
-export type { Props as ReplProps } from './Repl.vue'
-export type { OutputModes } from './types'
-export { version as languageToolsVersion } from '@vue/language-service/package.json'
+export * from './core'
