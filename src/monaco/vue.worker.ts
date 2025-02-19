331b60ee7f50ad5ebad2ec6b868b576d237e8023
黄明富 <212149997@qq.com>
2025-02-19 14:47:15 +0800

    style: code format

--- src/monaco/vue.worker.ts
+++ src/monaco/vue.worker.ts
@@ -100,7 +100,7 @@ self.onmessage = async (msg: MessageEvent<WorkerMessage>) => {
 
 async function importTsFromCdn(tsVersion: string) {
   const _module = globalThis.module
-    ; (globalThis as any).module = { exports: {} }
+  ;(globalThis as any).module = { exports: {} }
   const tsUrl = `https://cdn.jsdelivr.net/npm/typescript@${tsVersion}/lib/typescript.js`
   await import(/* @vite-ignore */ tsUrl)
   const ts = globalThis.module.exports
