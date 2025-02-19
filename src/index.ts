da105a42618899d214701a5fb6549719f73331bb
Noblet Ouways <91417411+Dsaquel@users.noreply.github.com>
2025-02-19 08:20:59 +0100

    feat: add core entry for node usage (#310)

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,5 @@
-export { default as Repl } from './Repl.vue'
+export { default as Repl, type Props as ReplProps } from './Repl.vue'
 export { default as Preview } from './output/Preview.vue'
-export { default as Sandbox } from './output/Sandbox.vue'
-export type { SandboxProps } from './output/Sandbox.vue'
-export {
-  useStore,
-  File,
-  type SFCOptions,
-  type StoreState,
-  type Store,
-  type ReplStore,
-} from './store'
-export { useVueImportMap, mergeImportMap, type ImportMap } from './import-map'
-export { compileFile } from './transform'
-export type { Props as ReplProps } from './Repl.vue'
+export { default as Sandbox, type SandboxProps } from './output/Sandbox.vue'
 export type { OutputModes } from './types'
-export { version as languageToolsVersion } from '@vue/language-service/package.json'
+export * from './core'
