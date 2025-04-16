83ba6836376b22a98d5b6d5725a1967dd6f7da24
Dsaquel <noblet.ouwaysgta5@gmail.com>
2025-04-16 23:05:05 +0200

    chore: tweaks

--- src/Repl.vue
+++ src/Repl.vue
@@ -9,10 +9,6 @@ import {
   injectKeyProps,
 } from './types'
 import EditorContainer from './editor/EditorContainer.vue'
-import 'luna-object-viewer/luna-object-viewer.css'
-import 'luna-data-grid/luna-data-grid.css'
-import 'luna-dom-viewer/luna-dom-viewer.css'
-import 'luna-console/luna-console.css'
 
 import type * as monaco from 'monaco-editor-core'
 
--- src/output/Output.vue
+++ src/output/Output.vue
@@ -50,7 +50,16 @@ function createConsole() {
   lunaConsole.value = new LunaConsole(consoleContainerRef.value, {
     theme: theme.value || 'light',
   })
-  lunaWatcher ??= watch(() => store.value.activeFile.code, clearLunaConsole)
+  if (!lunaWatcher) {
+    const consoleStyles = [
+      import('luna-object-viewer/luna-object-viewer.css'),
+      import('luna-data-grid/luna-data-grid.css'),
+      import('luna-dom-viewer/luna-dom-viewer.css'),
+      import('luna-console/luna-console.css'),
+    ]
+    Promise.all(consoleStyles)
+    lunaWatcher = watch(() => store.value.activeFile.code, clearLunaConsole)
+  }
   lunaWatcher.resume()
 }
 
