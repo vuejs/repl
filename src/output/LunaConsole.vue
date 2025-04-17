3d2d04e9472cf5bd6aa3a3b6f61e3fe16ca515ee
Dsaquel <noblet.ouwaysgta5@gmail.com>
2025-04-17 13:22:36 +0200

    chore: drop luna-dom-viewer

--- src/output/LunaConsole.vue
+++ src/output/LunaConsole.vue
@@ -4,7 +4,6 @@ import LunaConsole from 'luna-console'
 import { type LogPayload, injectKeyProps } from '../types'
 import 'luna-object-viewer/luna-object-viewer.css'
 import 'luna-data-grid/luna-data-grid.css'
-import 'luna-dom-viewer/luna-dom-viewer.css'
 import 'luna-console/luna-console.css'
 
 const { store, theme } = inject(injectKeyProps)!
