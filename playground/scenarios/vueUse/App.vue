266b4f7ccd55a121058592ee9a5fde84ef172057
zenheart <zenheart_register@163.com>
2025-05-16 12:16:56 +0800

    fix: add a VueUse demo

---
+++ playground/scenarios/vueUse/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="app-container">
+    <Coordinate label="X" :value="x" />
+    <Coordinate label="Y" :value="y" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { useMouse } from '@vueuse/core'
+import Coordinate from './Coordinate.vue'
+
+const { x, y } = useMouse()
+</script>
+
+<style scoped>
+.app-container {
+  display: grid;
+  grid-auto-flow: column;
+  gap: 1rem;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  font-family: monospace;
+}
+</style>
