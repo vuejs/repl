4688539ec3919d73e0b182fbd4b6c289c51613b1
Daniel Herman <dherman@factset.com>
2025-07-30 15:14:53 -0400

    fix: import

--- src/monaco/vue.worker.ts
+++ src/monaco/vue.worker.ts
@@ -3,7 +3,7 @@ import * as worker from 'monaco-editor-core/esm/vs/editor/editor.worker'
 import type * as monaco from 'monaco-editor-core'
 import {
   type LanguageServiceEnvironment,
-  createTypeScriptWorkerService,
+  createTypeScriptWorkerLanguageService,
 } from '@volar/monaco/worker'
 import { createNpmFileSystem } from '../unpkg/npm'
 import {
@@ -72,7 +72,7 @@ self.onmessage = async (msg: MessageEvent<WorkerMessage>) => {
         tsconfig.vueCompilerOptions || {},
       )
 
-      return createTypeScriptWorkerService({
+      return createTypeScriptWorkerLanguageService({
         typescript: ts,
         compilerOptions,
         workerContext: ctx,
